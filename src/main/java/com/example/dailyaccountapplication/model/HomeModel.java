7ec3101b411b915e56ac87ccac3058dc9e38392c
鄭翊宏 <>
2023-07-26 16:21:22 +0800

    update

--- src/main/java/com/example/dailyaccountapplication/model/HomeModel.java
+++ src/main/java/com/example/dailyaccountapplication/model/HomeModel.java
@@ -1,5 +1,13 @@
 package com.example.dailyaccountapplication.model;
 
+import com.example.dailyaccountapplication.core.Account;
+
 public class HomeModel {
+    private Account acc;
+
+    HomeModel(Account account){
+        acc = account;
+    }
+
 
 }
